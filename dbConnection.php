5257b9039e7d3c44d01320fe3ec016d0c60fd770
Murilo <Murisuzuki11@gmail.com>
2025-04-22 20:47:00 -0300

    Finalizando a aula

--- dbConnection.php
+++ dbConnection.php
@@ -3,11 +3,88 @@ try {
     $mysqli = new mysqli(
         "localhost", //endereco servidor
         "root",     //usuário do banco
-        "root",    //senha do banco
-        "2ds"     //schema/owner
+        "12345678",    //senha do banco
+        "db2ads"     //schema/owner
     );
 } catch (mysqli_sql_exception $e) {
+    echo('erro de conexão com o banco de dados');
 }
+
+//Este método esta preparado para executar no banco um select com retorno de diversas linhas
+/*$resultadoQuery = db_query('select * from 2ds.ds_usuario', $mysqli);
+if($resultadoQuery){
+    while($linha = $resultadoQuery->fetch_assoc()){
+        echo "estou dentro do loop";
+        echo "<br>";
+        echo $linha['usu_codigo'];
+        echo $linha['usu_senha'];
+        echo $linha['usu_ativo'];
+        echo "<br>";
+    }
+   ;   
+}else{
+    echo"Não deu certo o comando no banco";
+}
+*/
+
+//-----------------------------------------------------------------------------
+//Este metodo esta preparado para retornar uma linha determinada por um Where
+/*
+$resultadoQuery = db_query('select * from 2ds.ds_usuario where usu_codigo = "celsoxcunha"', $usuario $mysqli);
+if($resultadoQuery){
+    $linha = $resultadoQuery->fetch_assoc();
+        echo $linha['usu_codigo'];
+        echo $linha['usu_senha'];
+        echo $linha['usu_ativo'];
+        echo "<br>";  
+}else{
+    echo"Não deu certo o comando no banco";
+}
+*/
+
+
+//Este metodo esta preparado para realizar um Insert no banco de dados de acordo com o preenchimento das variaveis
+/*
+$usuario = 'nickname';
+$senha = '65748';
+$ativo = 'SSSSSSS';
+$resultadoQuery = db_query("insert into 2ds.ds_usuario 
+                           (usu_codigo,usu_senha,usu_ativo) 
+                          VALUES ('$usuario','$senha','$ativo')",$mysqli);
+
+if ($resultadoQuery){
+    echo "registro gravado no banco com sucesso";
+}else{
+    echo "Nao deu certo a gravação";
+}
+*/
+
+//Este metodo esta preparado para realzar um Update no banco de dados
+/*
+$usuario_old = 'nickname';
+$usuario_new = 'LewisHamilton';
+
+$resultadoQuery = db_query("Update 2ds.ds_usuario set usu_codigo = '$usuario_new' 
+                             where usu_codigo = '$usuario_old' ", $mysqli);
+
+if($resultadoQuery){
+    echo "registro Atualizado no banco com sucesso";
+
+}else{
+    echo "Nao deu certo a gravação";
+}
+*/
+
+
+// $usuario = 'LewisHamilton';
+
+// $resultadoQuery = db_query("delete from db2ads where usu_codigo = '$usuario'", $mysqli);
+
+// if($resultadoQuery){
+//     echo "registro apagado da historia da humanidade";
+// }else{
+//     echo "Oooops, ainda há esperança";
+// }
 
 //Este método esta preparado para executar no banco um select com retorno de diversas linhas
 /*$resultadoQuery = db_query('select * from 2ds.ds_usuario', $mysqli);
@@ -75,91 +152,15 @@ if($resultadoQuery){
 */
 
 
-$usuario = 'LewisHamilton';
+// $usuario = 'LewisHamilton';
 
-$resultadoQuery = db_query("delete from 2ds.ds_usuario where usu_codigo = '$usuario' ",$mysqli);
+// $resultadoQuery = db_query("delete from 2ds.ds_usuario where usu_codigo = '$usuario' ",$mysqli);
 
-if($resultadoQuery){
-    echo "registro apagado da historia da humanidade";
-}else{
-    echo "Oooops, ainda há esperança";
-}
-
-//Este método esta preparado para executar no banco um select com retorno de diversas linhas
-/*$resultadoQuery = db_query('select * from 2ds.ds_usuario', $mysqli);
-if($resultadoQuery){
-    while($linha = $resultadoQuery->fetch_assoc()){
-        echo "estou dentro do loop";
-        echo "<br>";
-        echo $linha['usu_codigo'];
-        echo $linha['usu_senha'];
-        echo $linha['usu_ativo'];
-        echo "<br>";
-    }
-   ;   
-}else{
-    echo"Não deu certo o comando no banco";
-}
-*/
-
-//-----------------------------------------------------------------------------
-//Este metodo esta preparado para retornar uma linha determinada por um Where
-/*
-$resultadoQuery = db_query('select * from 2ds.ds_usuario where usu_codigo = "celsoxcunha"', $mysqli);
-if($resultadoQuery){
-    $linha = $resultadoQuery->fetch_assoc();
-        echo $linha['usu_codigo'];
-        echo $linha['usu_senha'];
-        echo $linha['usu_ativo'];
-        echo "<br>";  
-}else{
-    echo"Não deu certo o comando no banco";
-}
-*/
-
-
-//Este metodo esta preparado para realizar um Insert no banco de dados de acordo com o preenchimento das variaveis
-/*
-$usuario = 'nickname';
-$senha = '65748';
-$ativo = 'SSSSSSS';
-$resultadoQuery = db_query("insert into 2ds.ds_usuario 
-                           (usu_codigo,usu_senha,usu_ativo) 
-                          VALUES ('$usuario','$senha','$ativo')",$mysqli);
-
-if ($resultadoQuery){
-    echo "registro gravado no banco com sucesso";
-}else{
-    echo "Nao deu certo a gravação";
-}
-*/
-
-//Este metodo esta preparado para realzar um Update no banco de dados
-/*
-$usuario_old = 'nickname';
-$usuario_new = 'LewisHamilton';
-
-$resultadoQuery = db_query("Update 2ds.ds_usuario set usu_codigo = '$usuario_new' 
-                             where usu_codigo = '$usuario_old' ", $mysqli);
-
-if($resultadoQuery){
-    echo "registro Atualizado no banco com sucesso";
-
-}else{
-    echo "Nao deu certo a gravação";
-}
-*/
-
-
-$usuario = 'LewisHamilton';
-
-$resultadoQuery = db_query("delete from 2ds.ds_usuario where usu_codigo = '$usuario' ",$mysqli);
-
-if($resultadoQuery){
-    echo "registro apagado da historia da humanidade";
-}else{
-    echo "Oooops, ainda há esperança";
-}
+// if($resultadoQuery){
+//     echo "registro apagado da historia da humanidade";
+// }else{
+//     echo "Oooops, ainda há esperança";
+// }
 
 
 function db_query($cmd, $conn)
--- processaLogin.php
+++ processaLogin.php
@@ -1,5 +1,37 @@
 <?php
-echo $_SERVER["REMOTE_ADDR"];
-echo $_POST["Usuario"];
-echo $_POST["Senha"];
+$user = $_POST["Usuario"];
+$senha = $_POST["Senha"];
+include("dbConnection.php");
+
+// echo($user);
+// echo($senha);
+
+try {
+    $mysqli = new mysqli(
+        "localhost", //endereco servidor
+        "root",     //usuário do banco
+        "12345678",    //senha do banco
+        "db2ads"     //schema/owner
+    );
+} catch (mysqli_sql_exception $e) {
+    echo('erro de conexão com o banco de dados');
+}
+
+$resultadoQuery = db_query("select * from tb_usuario where email = '$user' and senha = '$senha';", $mysqli);
+if($resultadoQuery){
+    if($resultadoQuery->num_rows > 0){
+        echo('deu certo');
+    }else{
+        echo('não deu certo');
+    }
+    $linha = $resultadoQuery->fetch_assoc();
+        // echo "deu certo";  
+}else{
+    echo"Não deu certo o comando no banco";
+}
+
+
+// echo $_SERVER["REMOTE_ADDR"];
+// echo $_POST["Usuario"];
+// echo $_POST["Senha"];
 ?>
